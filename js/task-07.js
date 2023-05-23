53d58c4d1d8d729918feea0ac401e79c9f672816
Иван-Т0 <vanyataran24@gmail.com>
2023-05-23 17:18:12 +0300

    update task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,10 @@
+
 const fontSizeControl = document.querySelector('#font-size-control');
 const textElement = document.querySelector('#text');
 
-fontSizeControl.addEventListener('input', () => {
-  const fontSize = fontSizeControl.value + 'px';
+textElement.style.fontSize = fontSizeControl.value + 'px';
+
+fontSizeControl.addEventListener('input', (event) => {
+  const fontSize = event.target.value + 'px';
   textElement.style.fontSize = fontSize;
 });
